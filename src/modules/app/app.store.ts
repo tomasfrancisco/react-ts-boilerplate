97de4f0e0167f65578c373191009628501d25bbf
Tomás Francisco <mail@tomasfrancisco.com>
2018-03-21 23:12:03 +0100

    add redux-saga dependency

--- src/modules/app/app.store.ts
+++ src/modules/app/app.store.ts
@@ -1,6 +1,9 @@
-import { combineReducers, createStore } from "redux";
 import { appReducer } from "./app.reducer";
+import createSagaMiddleware from "redux-saga";
+import { combineReducers, createStore, applyMiddleware } from "redux";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const reducer = combineReducers({ appReducer });
 
-export const appStore = createStore(reducer);
+export const appStore = createStore(reducer, applyMiddleware(sagaMiddleware));
